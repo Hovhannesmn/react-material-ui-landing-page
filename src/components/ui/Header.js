9cb1ab2864972e26a7862544645c14f8d3823076
Hovhannesmn <hovhannesmn@gmail.com>
2020-09-23 02:26:13 +0400

    add scroll element

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
+import { useScrollTrigger } from '@material-ui/core';
+
+const ElevationScroll = props => {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
 
 const Header = props => {
   return (
-    <AppBar position="fixed" color="red">
-      <ToolBar>
-        Arc Development
-      </ToolBar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position="fixed" >
+        <ToolBar>
+          Arc Development
+        </ToolBar>
+      </AppBar>
+    </ElevationScroll>
   );
 };
 
