7d8f3bfe6bb6c5bcb95a8b4ed7dc8bb1387c26ba
Hovhannesmn <hovhannesmn@gmail.com>
2020-09-23 02:38:37 +0400

    Add text

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from './ui/Header';
+import ToolBar from '@material-ui/core/Toolbar/Toolbar';
 
 const App = props => {
   return (
     <div>
       <Header />
+      {
+        [
+          ...new Array(120)
+        ].map(
+          () => ` In your terminal, navigate to the repo you want to make the changes in. Execute git config --list to check current username & email in your local repo.
+             Change username & email as desired. Make it a global change or specific to the local repo: git config [--global] user.name "Full Name" Done!`
+        ).join('\n')
+      }
       Hello
     </div>
   );
